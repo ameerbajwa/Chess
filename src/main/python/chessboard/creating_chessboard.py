35552989ab6b7b2ca73db15167f4a68db3d247ef
ameerbajwa <ameerbajwa@gmail.com>
2020-01-09 17:31:46 -0800

    Creating chessboard and defining all the chess pieces at the beginning of the game.

--- src/main/python/chessboard/creating_chessboard.py
+++ src/main/python/chessboard/creating_chessboard.py
@@ -32,12 +32,33 @@ def initialize_chess_pieces(chessboard):
     white_rook_2 = creating_chess_pieces.Rook('White', [7, 7])
     chessboard.loc[8, 'H'] = white_rook_2
 
+    white_pawns = []
+    black_pawns = []
+
+    for i in range(0,8):
+        white_pawns.append(creating_chess_pieces.Pawn('White', [6,i]))
+        chessboard.iloc[6,i] = white_pawns[i]
+        black_pawns.append(creating_chess_pieces.Pawn('Black', [1,i]))
+        chessboard.iloc[1,i] = black_pawns[i]
 
     black_king = creating_chess_pieces.King('Black', [0, 4])
     chessboard.loc[1, 'E'] = black_king
     black_queen = creating_chess_pieces.Queen('Black', [0, 3])
     chessboard.loc[1, 'D'] = black_queen
 
+    black_bishop_1 = creating_chess_pieces.Bishop('Black', [0, 2])
+    chessboard.loc[1, 'C'] = black_bishop_1
+    black_bishop_2 = creating_chess_pieces.Bishop('Black', [0, 5])
+    chessboard.loc[1, 'F'] = black_bishop_2
 
+    black_knight_1 = creating_chess_pieces.Knight('Black', [0, 1])
+    chessboard.loc[1, 'B'] = black_knight_1
+    black_knight_2 = creating_chess_pieces.Knight('Black', [0, 6])
+    chessboard.loc[1, 'G'] = black_knight_2
+
+    black_rook_1 = creating_chess_pieces.Rook('Black', [0, 0])
+    chessboard.loc[1, 'A'] = black_rook_1
+    black_rook_2 = creating_chess_pieces.Rook('White', [0, 7])
+    chessboard.loc[1, 'H'] = black_rook_2
 
     return (chessboard)
--- src/main/python/start_game.py
+++ src/main/python/start_game.py
@@ -7,11 +7,11 @@ chessboard = creating_chessboard.initialize_chessboard()
 chessboard_with_pieces = creating_chessboard.initialize_chess_pieces(chessboard)
 print (chessboard)
 
-player_1 = input('Who will be player_1? (ENTER "White" or "Black") ')
-if (player_1 == 'White'):
-    player_2 = 'Black'
-else:
-    player_2 = 'White'
+# player_1 = input('Who will be player_1? (ENTER "White" or "Black") ')
+# if (player_1 == 'White'):
+#     player_2 = 'Black'
+# else:
+#     player_2 = 'White'
 
 # game_finished = False
 #
