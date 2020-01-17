cc45ba491e4ad470ce6dc27b876ff68e2033a14e
ameerbajwa <ameerbajwa@gmail.com>
2020-01-16 17:46:19 -0800

    Defining the logic behind the chess piece moves and attacks.

--- src/main/python/chesspieces/chess_piece_moves.py
+++ src/main/python/chesspieces/chess_piece_moves.py
@@ -17,6 +17,7 @@ def move_chess_piece(chess_piece, chess_piece_position, move_to, chessboard, pla
 
     if chess_piece == 'Pawn':
         if (chessboard.iloc[chess_piece_location[0]-1, chess_piece_location[1]-1].chess_piece == 'Pawn'):
+            # and (chessboard.iloc[chess_piece_location[0]-1, chess_piece_location[1]-1].current_position[0] == chess_piece_location[0]-1) and (chessboard.iloc[chess_piece_location[0]-1, chess_piece_location[1]-1].current_position[1] == chess_piece_location[1]-1):
             print('Selected a Pawn')
             chessboard = move_pawn(chess_piece_location, move_to_location, chessboard, player)
         else:
@@ -27,29 +28,22 @@ def move_chess_piece(chess_piece, chess_piece_position, move_to, chessboard, pla
 
 
 def move_pawn(chess_piece_location, move_to_location, chessboard, player):
-    chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1] = chessboard.iloc[chess_piece_location[0]-1,
-                                                                                    chess_piece_location[1]-1]
-    chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1].current_position[0] = move_to_location[0]-1
-    chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1].current_position[1] = move_to_location[1]-1
-    chessboard.iloc[chess_piece_location[0]-1, chess_piece_location[1]-1] = '.'
 
-    # if player == 'White':
-    #     if (move_to_location[0] == 5) and (chessboard.iloc[5, move_to_location[1]-1] == '.'):
-    #         chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1] = chessboard.iloc[chess_piece_location[0]-1, chess_piece_location[1]-1]
-    #         chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1].current_location[0] = move_to_location[0]-1
-    #         chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1].current_location[1] = move_to_location[1]-1
-    #         chessboard.iloc[move_to_location[0]-3, move_to_location[1]-1] = '.'
-    #     else:
-    #         chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1] = chessboard.iloc[chess_piece_location[0]-1, chess_piece_location[1]-1]
-    #         chessboard.iloc[move_to_location[0]-2, move_to_location[1]-1] = '.'
-    #
-    # if player == 'Black':
-    #     if (move_to_location[0] == 4) and (chessboard.iloc[2, move_to_location[1]-1] == '.'):
-    #         chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1] = chessboard.iloc[chess_piece_location[0]-1, chess_piece_location[1]-1]
-    #         chessboard.iloc[move_to_location[0]+1, move_to_location[1]-1] = '.'
-    #     else:
-    #         chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1] = chessboard.iloc[chess_piece_location[0]-1, chess_piece_location[1]-1]
-    #         chessboard.iloc[move_to_location[0], move_to_location[1]-1] = '.'
+    new_loc_y = move_to_location[0]-1
+    new_loc_x = move_to_location[1]-1
+    old_loc_y = chess_piece_location[0]-1
+    old_loc_x = chess_piece_location[1]-1
+
+    blank_spot_condition = (chessboard.iloc[new_loc_y, new_loc_x] == '.')
+    move_1_spot = (old_loc_y-new_loc_y)**2 == 1 and (old_loc_x-new_loc_x)**2 == 0
+    move_2_spot = (old_loc_y-new_loc_y)**2 == 4 and (old_loc_x-new_loc_x)**2 == 0
+    attack = (old_loc_y-new_loc_y)**2 == 1 and (old_loc_x-new_loc_x)**2 == 1
+
+    if (player == 'White' and old_loc_y-new_loc_y == 1 and old_loc_x-new_loc_x == 0) or (player == 'Black' and new_loc_y-old_loc_y == 1 and old_loc_x-new_loc_x == 0):
+        chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1] = chessboard.iloc[chess_piece_location[0]-1,chess_piece_location[1]-1]
+        chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1].current_position[0] = move_to_location[0]-1
+        chessboard.iloc[move_to_location[0]-1, move_to_location[1]-1].current_position[1] = move_to_location[1]-1
+        chessboard.iloc[chess_piece_location[0]-1, chess_piece_location[1]-1] = '.'
 
     return chessboard
 
